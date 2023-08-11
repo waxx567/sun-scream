a69e222c665958b37653ece784f3d523ffd91547
waxx567 <waynem567@gmail.com>
2023-08-11 15:52:22 +0200

    Create routes.py

---
+++ backend/routes.py
@@ -0,0 +1,23 @@
+from flask import current_app,jsonify,request
+from app import create_app,db
+from models import Articles,articles_schema
+
+# Create an application instance
+app = create_app()
+
+# Define a route to fetch the avaialable articles
+
+@app.route("/articles", methods=["GET"], strict_slashes=False)
+def articles():
+
+	articles = Articles.query.all()
+	results = articles_schema.dump(articles)
+
+	return jsonify(results)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
+	
+
+# In the above snippet, we design a route that will collect the data, then serialize objects by giving them to the schema's dump method, which will provide the structured result. jsonify() produces a Response object with the mimetype application/json set.
